ea9cef7f50aa49eb9d16a88910c3052104b4ed61
Yuri Boschetto <47603437+Zoltek98@users.noreply.github.com>
2026-04-03 10:25:58 +0200

    Obscure bot token values in SolarBot.java
    Replaced sensitive token values with placeholders.

--- src/main/java/it/yboschetto/bot1/SolarBot.java
+++ src/main/java/it/yboschetto/bot1/SolarBot.java
@@ -72,8 +72,8 @@ public class SolarBot extends TelegramLongPollingBot {
 	@Value("${telegram.bot.username}")
 	private String BOTNAME;
 	
-	 private String token_test = "875502005:AAH7wBx1hsmmTio04RI4EgX9ZrDnY9QOl8g";
-	 private String token_prod = "814488899:AAE7n6sUnclziB26FMtuw8HOmy8EEVH0x0c";
+	 private String token_test = "875502005:xxx";
+	 private String token_prod = "814488899:xxx";
 	 
 	private String TOKEN = token_prod;
 	
